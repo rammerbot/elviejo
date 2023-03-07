095225d503b0815cabfefd84b96262fff5de9c99
rammerbot <rammer94@gmail.com>
2023-03-07 16:07:08 +0000

    elviejo in prod

--- blog/settings/local.py
+++ blog/settings/local.py
@@ -3,7 +3,7 @@ from .base import *
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
@@ -27,6 +27,7 @@ DATABASES = {
 
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [(os.path.join(BASE_DIR, 'static'))]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 MEDIA_URL = 'media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
