d3d55af3eb8ff3c0f56d10674b1969b8e5dd367e
unknown <rammer94@gmail.com>
2024-01-14 11:25:58 -0400

    prod correcion alloweds

--- blog/settings/prod.py
+++ blog/settings/prod.py
@@ -3,7 +3,9 @@ from .base import *
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['.rammerbot.com']
+CSRF_TRUSTED_ORIGINS=['https://serviexpo.rammerbot.com']
+
 
 # Application definition
 
