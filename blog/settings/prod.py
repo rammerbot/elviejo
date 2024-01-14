3ebd294059f5484e1eb5e716f34d100191821ad4
unknown <rammer94@gmail.com>
2024-01-14 11:08:35 -0400

    actualizacion para relanzamiento

--- blog/settings/prod.py
+++ blog/settings/prod.py
@@ -1,7 +1,7 @@
 
 from .base import *
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
@@ -27,6 +27,7 @@ DATABASES = {
 
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [(os.path.join(BASE_DIR, 'static'))]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 MEDIA_URL = 'media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
